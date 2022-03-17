18f90d2973e7d771b984be302c0bf0ab6992ed77
MarkIan1996 <marcoianni1996@gmail.com>
2022-03-17 11:56:10 +0100

    Modifica nome immagini e aggiunta voce menu attiva e allo scroll

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+const sections = document.querySelectorAll(".section");
+const navLi = document.querySelectorAll("nav ul li");
+console.log(sections);
+console.log(navLi);
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    console.log(li);
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+});
