db309700f8cb8e674c5735c08753c60a84a77e61
MarkIan1996 <marcoianni1996@gmail.com>
2022-03-18 17:43:26 +0100

    sistemata hero image, scroll to top, pulizia css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 
+//--------------------- HAMBURGER MENU ---------------------
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", mobileMenu);
@@ -15,6 +16,9 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+
+//--------------------- ACTIVE MENU LINK ---------------------
 
 const sections = document.querySelectorAll(".section");
 const navLi = document.querySelectorAll("nav ul li");
@@ -38,3 +42,25 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+//--------------------- SCROLL TO TOP ---------------------
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "flex";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
